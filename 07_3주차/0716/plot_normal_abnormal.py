fd629910f1e06c8e24f2d3a076227646dc93e2c9
KimGiHu <88769506+KimGiHu@users.noreply.github.com>
2024-07-16 17:49:20 +0900

    Create plot_normal_abnormal.py

---
+++ 07_3주차/0716/plot_normal_abnormal.py
@@ -0,0 +1,98 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import random
+from model import *
+
+# 고유 파형들 정의
+features=['A+IGBT-I', 'A+*IGBT-I', 'B+IGBT-I', 'B+*IGBT-I', 'C+IGBT-I', 
+          'C+*IGBT-I', 'A-FLUX', 'B-FLUX', 'C-FLUX', 'CB-I','CB-V',
+          'MOD-V', 'MOD-I','DV/DT']
+
+# 모듈별 변수들 정의
+system1='RFQ'
+system2='DTL'
+system3='CCL'
+system4='SCL'
+
+# 데이터 로드 및 라벨 인코딩
+def load_data(system):
+    X = np.load(f'./hvcm/data/hvcm/{system}.npy')
+    Y = np.load(f'./hvcm/data/hvcm/{system}_labels.npy', allow_pickle=True)
+    return X, Y
+
+X1, Y1 = load_data(system1)
+X2, Y2 = load_data(system2)
+X3, Y3 = load_data(system3)
+X4, Y4 = load_data(system4)
+
+time = np.arange(X1.shape[1]) * 400e-9 # 타임스텝 : 1.8ms (4500개 샘플씩, 한 샘플당 400ns)
+
+# 시스템 인덱스 생성
+system_indices = {
+    system1: 0,
+    system2: 1,
+    system3: 2,
+    system4: 3
+}
+
+def create_labels(Y, system):
+    labels = np.array([system_indices[system]] * len(Y))
+    return labels
+
+labels1 = create_labels(Y1, system1)
+labels2 = create_labels(Y2, system2)
+labels3 = create_labels(Y3, system3)
+labels4 = create_labels(Y4, system4)
+
+# 배열 X,Y의 정상 및 오류 데이터들을 분리함
+fault_indices_RFQ, normal_indices_RFQ = np.where(Y1[:,1] == 'Fault')[0], np.where(Y1[:,1] == 'Run')[0] 
+fault_indices_DTL, normal_indices_DTL = np.where(Y2[:,1] == 'Fault')[0], np.where(Y2[:,1] == 'Run')[0]
+fault_indices_CCL, normal_indices_CCL = np.where(Y3[:,1] == 'Fault')[0], np.where(Y3[:,1] == 'Run')[0]
+fault_indices_SCL, normal_indices_SCL = np.where(Y4[:,1] == 'Fault')[0], np.where(Y4[:,1] == 'Run')[0]
+
+Xnormal_RFQ, Xanomaly_RFQ = X1[normal_indices_RFQ,:,:], X1[fault_indices_RFQ,:,:]
+Xnormal_DTL, Xanomaly_DTL = X2[normal_indices_DTL,:,:], X2[fault_indices_DTL,:,:]
+Xnormal_CCL, Xanomaly_CCL = X3[normal_indices_CCL,:,:], X3[fault_indices_CCL,:,:]
+Xnormal_SCL, Xanomaly_SCL = X4[normal_indices_SCL,:,:], X4[fault_indices_SCL,:,:]
+
+Ynormal_RFQ, Yanomaly_RFQ = Y1[normal_indices_RFQ,:], Y1[fault_indices_RFQ,:]
+Ynormal_DTL, Yanomaly_DTL = Y2[normal_indices_DTL,:], Y2[fault_indices_DTL,:]
+Ynormal_CCL, Yanomaly_CCL = Y3[normal_indices_CCL,:], Y3[fault_indices_CCL,:]
+Ynormal_SCL, Yanomaly_SCL = Y4[normal_indices_SCL,:], Y4[fault_indices_SCL,:]
+
+# Min-Max 스케일러 인스턴스 초기화
+# scaler = MinMaxScaler()
+scaler_IGBT = MinMaxScaler()
+scaler_FLUX = MinMaxScaler()
+scaler_CAP = MinMaxScaler()
+scaler_MOD = MinMaxScaler()
+
+Xnormal_concat = np.concatenate( (Xnormal_RFQ[:524,:,:], Xnormal_DTL[:524,:,:], Xnormal_CCL[:524,:,:], Xnormal_SCL[:524,:,:]), axis=0 )
+Xnormal_concat = np.array(Xnormal_concat)
+
+# 정상신호 N개 학습
+data1 = Xnormal_concat[:,:,:] # 4가지 모듈(RFQ, DTL, CCL, SCL) 정보를 가진524 * 4 = 2096개의 데이터를 학습함. 
+
+# Flux Low Fault
+column_to_search_RFQ = Yanomaly_RFQ[:, 2].astype(str)
+
+flux_FAULT_RFQ = np.where(np.char.find(column_to_search_RFQ, 'A FLUX') != -1)[0]
+tmp_FLUX_RFQ = len(flux_FAULT_RFQ) 
+
+normal = Xnormal_concat[0,:,6]
+abnormal = Xanomaly_RFQ[flux_FAULT_RFQ[0],:,6]
+
+plt.figure(figsize=(12, 6))
+plt.plot(normal[:], label="Normal")
+plt.plot(abnormal[:], label="Fault")
+plt.legend()
+plt.xlabel("Time (s)")
+plt.ylabel("A Flux")
+plt.title("Normal vs fault")
+
+plt.savefig('./figure/0716//' + 'normal vs. abnormal' + '.png', dpi=600)
